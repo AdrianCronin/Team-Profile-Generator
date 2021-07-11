4d9c0dcdf2ef9992aab4310dc15d3be8d419b242
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-11 15:13:25 -0700

    created functions to start prompting the user and successfully generate a valid html file

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const inquirer = require('inquirer'); // inquirer module
 const fs = require('fs'); // file system module
+const generateHTML = require('./src/generateHTML'); // import generated HTML string
 
 // takes the HTML data and creates a new index.html file
 // TODO: probably need to make a css file too
 const writeHTMLFile = (data) => {
-    fs.writeFile(`./dist/index.html`, data, (err) => 
+    fs.writeFile(`./dist/index.html`, data, (err) =>
         err ? console.log(err) : console.log(`HTML File created successfully!`))
 };
+
+const init = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?',
+                name: 'name',
+            }
+        ])
+        .then((response) =>
+            generateHTML(response)
+        )
+        .then((response) =>
+            writeHTMLFile(response)
+        );
+}
+
+init();
---
+++ src/generateHTML.js
@@ -0,0 +1,25 @@
+
+// return a string of HTML
+const generateHTML = (data) => {
+    console.log(data);
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>${data.name}</title>
+    </head>
+
+    <body>
+    <h1>${data.name}</h1>
+    </body>
+
+    </html>
+    `
+};
+
+module.exports = generateHTML;
