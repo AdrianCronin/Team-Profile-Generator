7e8de3070024e44faa30b86b3ca78c4efa6c5619
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-17 08:37:57 -0700

    created unit tests to verify the the creation of Employee class objects and the functionality of its methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,49 @@
 const Employee = require('../lib/employee');
 
+describe("Employee", () => {
+    const name = 'Bob';
+    const id = 1;
+    const email = 'Bob@gmail.com';
+    
+    // Verify an Employee is created with the correct property values from the given arguments
+    describe("Initialization", () => {
+        it("Should create a new Employee object with name 'Bob' id '1' and email 'Bob@gmail.com'", () => {
+            const employee  = new Employee(name, id, email);
+            expect(employee).toEqual({name: name, id: id, email: email, role: 'Employee'});
+        });
+
+    });
+
+    // Verify `getName()` function returns the Employee's name
+    describe("getName", () => {
+        it("Should return the Employee object's name 'Bob'", () => {
+            const employee  = new Employee(name, id, email);
+            expect(employee.getName()).toEqual(name);
+        });
+    });
+
+    // Verify `getId()` function returns the Employee's id
+    describe("getId", () => {
+        it("Should return the Employee object's id of '1'", () => {
+            const employee  = new Employee(name, id, email);
+            expect(employee.getId()).toEqual(id);    
+        });
+    });
+
+    // Verify `getEmail()` function returns the Employee's email
+    describe("getEmail", () => {
+        it("Should return the Employee object's email of 'Bob@gmail.com'", () => {
+            const employee  = new Employee(name, id, email);
+            expect(employee.getEmail()).toEqual(email);    
+        });
+    });
+
+    // Verify `getRole()` function returns the Employee's role
+    describe("getRole", () => {
+        it("Should return the Employee object's role of 'Employee'", () => {
+            const employee  = new Employee(name, id, email);
+            expect(employee.getRole()).toEqual("Employee");    
+        });
+    });
+
+});
