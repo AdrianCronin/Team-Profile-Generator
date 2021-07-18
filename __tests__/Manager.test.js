d1092c719141c10376f37666c16d295d8ac538df
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-17 21:37:55 -0700

    created unit tests for the Manager subclass

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -8,15 +8,48 @@ describe("Manager", () => {
 
     describe("Initialization", () => {
         it("Should create a new Manager subclass object with name: 'Tiffany' id: '123' email: 'Tiffany@ManagerMail.com' office number: '1' and role: 'Manager' ", () => {
-            const manager = new Manager({name, id, email, officeNumber});
-            expect(manager).toEqual({name: name, id: id, email: email, officeNumber: officeNumber, role: 'Manager'});
-            
-            // expect(manager.name).toEqual(name);
-            // expect(manager.id).toEqual(id);
-            // expect(manager.email).toEqual(email);
-            // expect(manager.officeNumber).toEqual(officeNumber);
-            // expect(manager.role).toEqual('Manager');
+            const manager = new Manager({ name, id, email, officeNumber });
+            expect(manager).toEqual({ name: name, id: id, email: email, officeNumber: officeNumber, role: 'Manager' });
         });
     });
 
+    // Verify `getName()` function returns the Manager's name
+    describe("getName", () => {
+        it("Should return the Manager object's name: 'Tiffany'", () => {
+            const manager = new Manager({ name, id, email, officeNumber });
+            expect(manager.getName()).toEqual(name);
+        });
+    });
+
+    // Verify `getId()` function returns the Manager's id
+    describe("getId", () => {
+        it("Should return the Employee object's id: '123'", () => {
+            const manager = new Manager({ name, id, email, officeNumber });
+            expect(manager.getId()).toEqual(id);
+        });
+    });
+
+    // Verify `getEmail()` function returns the Manager's email
+    describe("getEmail", () => {
+        it("Should return the Employee object's email: 'Tiffany@ManagerMail.com'", () => {
+            const manager = new Manager({ name, id, email, officeNumber });
+            expect(manager.getEmail()).toEqual(email);
+        });
+    });
+
+    // Verify `getRole()` function returns the Manager's role
+    describe("getRole", () => {
+        it("Should return the Employee object's role: 'Manager'", () => {
+            const employee = new Manager({ name, id, email, officeNumber });
+            expect(employee.getRole()).toEqual("Manager");
+        });
+    });
+
+    // Verify `getOfficeNumber()` function returns the Manager's office number
+    describe("getOfficeNumber", () => {
+        it("Should return the Manager object's office number: '1'", () => {
+            const employee = new Manager({ name, id, email, officeNumber });
+            expect(employee.getOfficeNumber()).toEqual(officeNumber);
+        });
+    });
 });
